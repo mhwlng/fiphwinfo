e59c3f9d9da4a52058c402fd91b035eac979b8a1
Marcel Houweling <mhwlng@gmail.com>
2021-01-23 15:47:21 +0100

    Add project files.

---
+++ fiphwinfo/App.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Hardcodet.Wpf.TaskbarNotification;
+using log4net;
+using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+using TheArtOfDev.HtmlRenderer.Core;
+using SharpDX.DirectInput;
+using System.Collections.Specialized;
+using System.Linq;
+
+
+// ReSharper disable StringLiteralTypo
+
+namespace fiphwinfo
+{
+
+    /// <summary>
+    /// Simple application. Check the XAML for comments.
+    /// </summary>
+    public partial class App : Application
+    {
+        public static bool IsShuttingDown { get; set; }
+
+
+        public static Task HWInfoTask;
+        private static CancellationTokenSource _hwInfoTokenSource = new CancellationTokenSource();
+
+        private static Mutex _mutex;
+
+        private TaskbarIcon _notifyIcon;
+
+        public static readonly FipHandler FipHandler = new FipHandler();
+
+
+        public static readonly ILog Log =
+            LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static CssData CssData;
+        
+        public static string ExePath;
+
+        private static CachedSound _clickSound = null;
+
+        public static void PlayClickSound()
+        {
+            if (_clickSound != null)
+            {
+                try
+                {
+                    AudioPlaybackEngine.Instance.PlaySound(_clickSound);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error( $"PlaySound: {ex}");
+                }
+            }
+        }
+
+        private static void GetExePath()
+        {
+            var strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            ExePath = Path.GetDirectoryName(strExeFilePath);
+        }
+
+        private static void RunProcess(string fileName)
+        {
+            var process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = fileName;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            process.WaitForExit();
+        }
+
+
+        protected override void OnStartup(StartupEventArgs evtArgs)
+        {
+            const string appName = "fiphwinfo";
+
+            _mutex = new Mutex(true, appName, out var createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Current.Shutdown();
+            }
+
+            GetExePath();
+
+            base.OnStartup(evtArgs);
+
+            log4net.Config.XmlConfigurator.Configure();
+
+           
+            _clickSound = null;
+
+            if (File.Exists(Path.Combine(ExePath, "appSettings.config")) &&
+                ConfigurationManager.GetSection("appSettings") is NameValueCollection appSection)
+            {
+                if (File.Exists(Path.Combine(ExePath, "Sounds", appSection["clickSound"])))
+                {
+                    try
+                    {
+                        _clickSound = new CachedSound(Path.Combine(ExePath, "Sounds", appSection["clickSound"]));
+                    }
+                    catch (Exception ex)
+                    {
+                        _clickSound = null;
+
+                        Log.Error($"CachedSound: {ex}");
+                    }
+
+                }
+            }
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+
+            _notifyIcon.IconSource = new BitmapImage(new Uri("pack://application:,,,/fiphwinfo;component/fiphwinfo.ico"));
+            _notifyIcon.ToolTipText = "fiphwinfo";
+
+            var splashScreen = new SplashScreenWindow();
+            splashScreen.Show();
+
+            Task.Run(() =>
+            {
+                var config = new TemplateServiceConfiguration
+                {
+                    TemplateManager = new ResolvePathTemplateManager(new[] { Path.Combine(ExePath, "Templates") }),
+                    DisableTempFileLocking = true,
+                    BaseTemplateType = typeof(HtmlSupportTemplateBase<>) /*,
+                    Namespaces = new HashSet<string>(){
+                        "System",
+                        "System.Linq",
+                        "System.Collections",
+                        "System.Collections.Generic"
+                        }*/
+                };
+
+                splashScreen.Dispatcher.Invoke(() => splashScreen.ProgressText.Text = "Loading cshtml templates...");
+
+                Engine.Razor = RazorEngineService.Create(config);
+
+                Engine.Razor.Compile("cardcaption.cshtml", null);
+                Engine.Razor.Compile("layout.cshtml", null);
+
+                Engine.Razor.Compile("hwinfo.cshtml", null);
+
+                CssData = TheArtOfDev.HtmlRenderer.WinForms.HtmlRender.ParseStyleSheet(
+                    File.ReadAllText(Path.Combine(ExePath, "Templates\\styles.css")), true);
+                
+                splashScreen.Dispatcher.Invoke(() => splashScreen.ProgressText.Text = "Getting sensor data from HWInfo...");
+
+                HWInfo.ReadMem("HWINFO.INC");
+                
+                if (HWInfo.SensorData.Any())
+                {
+                    HWInfo.SaveDataToFile(@"Data\hwinfo.json");
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    var window = Current.MainWindow = new MainWindow();
+                    window.ShowActivated = false;
+                });
+
+
+                splashScreen.Dispatcher.Invoke(() => splashScreen.ProgressText.Text = "Initializing FIP...");
+                if (!FipHandler.Initialize())
+                {
+                    Current.Shutdown();
+                }
+
+                Log.Info("fiphwinfo started");
+
+                Dispatcher.Invoke(() =>
+                {
+                    var window = Current.MainWindow;
+                    window?.Hide();
+                });
+
+                Dispatcher.Invoke(() => { splashScreen.Close(); });
+                
+                var hwInfoToken = _hwInfoTokenSource.Token;
+
+                HWInfoTask = Task.Run(async () =>
+                {
+                    Log.Info("HWInfo task started");
+
+                    while (true)
+                    {
+                        if (hwInfoToken.IsCancellationRequested)
+                        {
+                            hwInfoToken.ThrowIfCancellationRequested();
+                        }
+
+                        HWInfo.ReadMem("HWINFO.INC");
+
+                        FipHandler.RefreshHWInfoPages();
+
+                        await Task.Delay(5 * 1000, _hwInfoTokenSource.Token); // repeat every 5 seconds
+                    }
+
+                }, hwInfoToken);
+
+            });
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            FipHandler.Close();
+
+            _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+
+            _hwInfoTokenSource.Cancel();
+
+            var hwInfoToken = _hwInfoTokenSource.Token;
+
+            try
+            {
+                HWInfoTask?.Wait(hwInfoToken);
+            }
+            catch (OperationCanceledException)
+            {
+                Log.Info("HWInfo background task ended");
+            }
+            finally
+            {
+                _hwInfoTokenSource.Dispose();
+            }
+
+
+            Log.Info("exiting");
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ fiphwinfo/FipPanel.cs
@@ -0,0 +1,555 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Media.Imaging;
+using TheArtOfDev.HtmlRenderer.WinForms;
+using RazorEngine;
+using RazorEngine.Templating;
+using RazorEngine.Text;
+using TheArtOfDev.HtmlRenderer.Core.Entities;
+using Image = System.Drawing.Image;
+
+// For extension methods.
+
+
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+// ReSharper disable CommentTypo
+
+namespace fiphwinfo
+{
+   
+    public class MyHtmlHelper
+    {
+        public IEncodedString Raw(string rawString)
+        {
+            return new RawString(rawString);
+        }
+    }
+
+    public abstract class HtmlSupportTemplateBase<T> : TemplateBase<T>
+    {
+        public HtmlSupportTemplateBase()
+        {
+            Html = new MyHtmlHelper();
+        }
+
+        public MyHtmlHelper Html { get; set; }
+    }
+
+    internal class FipPanel
+    {
+
+        private readonly object _refreshDevicePageLock = new object();
+
+        private bool _initOk;
+
+        private int CurrentCard = 0;
+
+        private const int DEFAULT_PAGE = 0;
+
+        private int _currentLcdYOffset;
+        private int _currentLcdHeight;
+
+        public IntPtr FipDevicePointer;
+        private string SerialNumber;
+
+        private uint _prevButtons;
+
+        private bool[] _ledState = new bool[7];
+
+        private List<uint> _pageList = new List<uint>();
+
+        private readonly Pen _scrollPen = new Pen(Color.FromArgb(0xff,0xFF,0xB0,0x00));
+        private readonly Pen _whitePen = new Pen(Color.FromArgb(0xff, 0xFF, 0xFF, 0xFF),(float)0.1);
+        
+        
+        private readonly SolidBrush _scrollBrush = new SolidBrush(Color.FromArgb(0xff, 0xFF, 0xB0, 0x00));
+        private readonly SolidBrush _whiteBrush = new SolidBrush(Color.FromArgb(0xff, 0xFF, 0xFF, 0xFF));
+
+        private readonly Font _drawFont = new Font("Arial", 13, GraphicsUnit.Pixel);
+
+        private Image _htmlImage;
+        private Image _cardcaptionHtmlImage;
+
+        private const int HtmlWindowXOffset = 1;
+
+        private int _htmlWindowWidth = 320;
+        private int _htmlWindowHeight = 240;
+
+        private int HtmlWindowUsableWidth => _htmlWindowWidth - 9 - HtmlWindowXOffset;
+
+        private double ScrollBarHeight => _htmlWindowHeight -7.0;
+
+        private int ChartImageDisplayWidth => _htmlWindowWidth - 25;
+
+        private const int ChartImageDisplayHeight = 60;
+        
+        private DirectOutputClass.PageCallback _pageCallbackDelegate;
+        private DirectOutputClass.SoftButtonCallback _softButtonCallbackDelegate;
+
+        private bool _blockNextUpState;
+
+
+        public FipPanel(IntPtr devicePtr) 
+        {
+            FipDevicePointer = devicePtr;
+        }
+        
+        public void Initalize()
+        {
+            // FIP = 3e083cd8-6a37-4a58-80a8-3d6a2c07513e
+
+            // https://github.com/Raptor007/Falcon4toSaitek/blob/master/Raptor007's%20Falcon%204%20to%20Saitek%20Utility/DirectOutput.h
+            //https://github.com/poiuqwer78/fip4j-core/tree/master/src/main/java/ch/poiuqwer/saitek/fip4j
+
+            _pageCallbackDelegate = PageCallback;
+            _softButtonCallbackDelegate = SoftButtonCallback;
+
+            var returnValues1 = DirectOutputClass.RegisterPageCallback(FipDevicePointer, _pageCallbackDelegate);
+            if (returnValues1 != ReturnValues.S_OK)
+            {
+                App.Log.Error("FipPanel failed to init RegisterPageCallback. " + returnValues1);
+            }
+            var returnValues2 = DirectOutputClass.RegisterSoftButtonCallback(FipDevicePointer, _softButtonCallbackDelegate);
+            if (returnValues2 != ReturnValues.S_OK)
+            {
+                App.Log.Error("FipPanel failed to init RegisterSoftButtonCallback. " + returnValues1);
+            }
+
+            var returnValues3 = DirectOutputClass.GetSerialNumber(FipDevicePointer, out SerialNumber);
+            if (returnValues3 != ReturnValues.S_OK)
+            {
+                App.Log.Error("FipPanel failed to get Serial Number. " + returnValues1);
+            }
+            else
+            {
+                App.Log.Info("FipPanel Serial Number : " + SerialNumber);
+
+                _initOk = true;
+
+                AddPage(DEFAULT_PAGE, true);
+
+                RefreshDevicePage();
+            }
+
+        }
+
+        public void Shutdown()
+        {
+            try
+            {
+                if (_pageList.Count > 0)
+                {
+                    do
+                    {
+                        if (_initOk)
+                        {
+                            DirectOutputClass.RemovePage(FipDevicePointer, _pageList[0]);
+                        }
+
+                        _pageList.Remove(_pageList[0]);
+
+
+                    } while (_pageList.Count > 0);
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Log.Error(ex);
+            }
+
+        }
+
+        private void PageCallback(IntPtr device, IntPtr page, byte bActivated, IntPtr context)
+        {
+            if (device == FipDevicePointer)
+            {
+                if (bActivated != 0)
+                {
+                    RefreshDevicePage();
+                }
+            }
+        }
+
+        private void SoftButtonCallback(IntPtr device, IntPtr buttons, IntPtr context)
+        {
+            if (device == FipDevicePointer & (uint) buttons != _prevButtons)
+            {
+                var button = (uint) buttons ^ _prevButtons;
+                var state = ((uint) buttons & button) == button;
+                _prevButtons = (uint) buttons;
+
+                //Console.WriteLine($"button {button}  state {state}");
+
+                var mustRefresh = false;
+
+                var mustRender = true;
+
+                switch (button)
+                {
+                    case 8: // scroll clockwise
+                        if (state)
+                        {
+
+                            CurrentCard++;
+                            _currentLcdYOffset = 0;
+
+                            mustRefresh = true;
+
+                            var playSound = true;
+
+
+                            if (playSound)
+                            {
+                                App.PlayClickSound();
+                            }
+                        }
+
+                        break;
+                    case 16: // scroll anti-clockwise
+
+                        if (state)
+                        {
+                            CurrentCard--;
+                            _currentLcdYOffset = 0;
+
+                            mustRefresh = true;
+
+                            var playSound = true;
+
+                            if (playSound)
+                            {
+                                App.PlayClickSound();
+                            }
+                        }
+
+                        break;
+                    case 2: // scroll clockwise
+                        _currentLcdYOffset += 50;
+
+                        mustRender = false;
+
+                        mustRefresh = true;
+
+                        break;
+                    case 4: // scroll anti-clockwise
+
+                        if (_currentLcdYOffset == 0) return;
+
+                        _currentLcdYOffset -= 50;
+                        if (_currentLcdYOffset < 0)
+                        {
+                            _currentLcdYOffset = 0;
+                        }
+
+                        mustRender = false;
+
+                        mustRefresh = true;
+
+                        break;
+                }
+
+                if (!mustRefresh)
+                {
+                    if (state || !_blockNextUpState)
+                    {
+                        switch (button)
+                        {
+                            case 512:
+
+                                CurrentCard++;
+                                _currentLcdYOffset = 0;
+
+                                mustRefresh = true;
+
+                                App.PlayClickSound();
+
+                                break;
+                            case 1024:
+
+                                CurrentCard--;
+                                _currentLcdYOffset = 0;
+
+                                mustRefresh = true;
+
+                                App.PlayClickSound();
+
+                                break;
+                        }
+                    }
+
+                }
+
+                _blockNextUpState = state;
+
+                if (mustRefresh)
+                {
+                    RefreshDevicePage(mustRender);
+                }
+
+            }
+        }
+
+        private void CheckLcdOffset()
+        {
+            if (_currentLcdHeight <= _htmlWindowHeight)
+            {
+                _currentLcdYOffset = 0;
+            }
+
+            if (_currentLcdYOffset + _htmlWindowHeight > _currentLcdHeight )
+            {
+                _currentLcdYOffset = _currentLcdHeight - _htmlWindowHeight + 4;
+            }
+
+            if (_currentLcdYOffset < 0) _currentLcdYOffset = 0;
+        }
+
+        private ReturnValues AddPage(uint pageNumber, bool setActive)
+        {
+            var result = ReturnValues.E_FAIL;
+
+            if (_initOk)
+            {
+                try
+                {
+                    if (_pageList.Contains(pageNumber))
+                    {
+                        return ReturnValues.S_OK;
+                    }
+
+                    result = DirectOutputClass.AddPage(FipDevicePointer, (IntPtr) pageNumber, string.Concat("0x", FipDevicePointer.ToString(), " PageNo: ", pageNumber), setActive);
+                    if (result == ReturnValues.S_OK)
+                    {
+                        App.Log.Info("Page: " + pageNumber + " added");
+
+                        _pageList.Add(pageNumber);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    App.Log.Error(ex);
+                }
+            }
+
+            return result;
+        }
+
+        private ReturnValues SendImageToFip(uint page, Bitmap fipImage)
+        {
+
+            if (_initOk)
+            {
+                if (fipImage == null)
+                {
+                    return ReturnValues.E_INVALIDARG;
+                }
+
+                try
+                {
+                    fipImage.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+                    var bitmapData =
+                        fipImage.LockBits(new Rectangle(0, 0, fipImage.Width, fipImage.Height),
+                            ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                    var intPtr = bitmapData.Scan0;
+                    var local3 = bitmapData.Stride * fipImage.Height;
+                    DirectOutputClass.SetImage(FipDevicePointer, page, 0, local3, intPtr);
+                    fipImage.UnlockBits(bitmapData);
+                    return ReturnValues.S_OK;
+                }
+                catch (Exception ex)
+                {
+                    App.Log.Error(ex);
+                }
+            }
+
+            return ReturnValues.E_FAIL;
+        }
+        
+        private void OnImageLoad(object sender, HtmlImageLoadEventArgs e)
+        {
+
+            try
+            {
+                var image = new Bitmap(ChartImageDisplayWidth, ChartImageDisplayHeight);
+
+                using (var graphics = Graphics.FromImage(image))
+                {
+                    if (HWInfo.SensorTrends.ContainsKey(e.Src))
+                    {
+                        graphics.DrawLines(_scrollPen, HWInfo.SensorTrends[e.Src].Read(ChartImageDisplayWidth, ChartImageDisplayHeight));
+                    }
+
+                    graphics.DrawRectangle(_whitePen,
+                        new Rectangle(0, 0, ChartImageDisplayWidth - 1, ChartImageDisplayHeight - 1));
+
+                    graphics.DrawString(HWInfo.SensorTrends[e.Src].MaxV(), _drawFont, _whiteBrush, (float)1, (float)1);
+
+
+                    graphics.DrawString(HWInfo.SensorTrends[e.Src].MinV(), _drawFont, _whiteBrush, (float)1, (float)ChartImageDisplayHeight-17);
+                }
+
+                e.Callback(image);
+
+            }
+            catch
+            {
+                var image = new Bitmap(1, 1);
+
+                e.Callback(image);
+            }
+        }
+
+
+        private void SetLed(uint ledNumber, bool state)
+        {
+            if (_ledState[ledNumber] != state)
+            {
+                DirectOutputClass.SetLed(FipDevicePointer, DEFAULT_PAGE,
+                    ledNumber, state);
+                _ledState[ledNumber] = state;
+            }
+        }
+
+        public void RefreshDevicePage(bool mustRender = true)
+        {
+
+            lock (_refreshDevicePageLock)
+            {
+                using (var fipImage = new Bitmap(_htmlWindowWidth, _htmlWindowHeight))
+                {
+                    using (var graphics = Graphics.FromImage(fipImage))
+                    {
+                        var str = "";
+
+                        if (CurrentCard < 0)
+                        {
+                            CurrentCard = 1;
+                        }
+                        else
+                        if (CurrentCard > 1)
+                        {
+                            CurrentCard = 0;
+                        }
+
+                        if (mustRender)
+                        {
+                            try
+                            {
+                                lock (HWInfo.RefreshHWInfoLock)
+                                {
+                                    str =
+                                        Engine.Razor.Run("hwinfo.cshtml", null, new
+                                        {
+                                            CurrentCard = CurrentCard,
+
+                                            SensorCount = HWInfo.SensorData.Count,
+
+                                            SensorData = HWInfo.SensorData.Values.ToList(),
+
+                                            ChartImageDisplayWidth = ChartImageDisplayWidth,
+                                            ChartImageDisplayHeight = ChartImageDisplayHeight
+
+                                        });
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                App.Log.Error(ex);
+                            }
+                        }
+
+                        graphics.Clear(Color.Black);
+
+                        if (mustRender)
+                        {
+                            var measureData =HtmlRender.Measure(graphics, str, HtmlWindowUsableWidth, App.CssData,null, OnImageLoad);
+
+                            _currentLcdHeight = (int)measureData.Height;
+                        }
+
+                        CheckLcdOffset();
+
+                        if (_currentLcdHeight > 0)
+                        {
+
+                            if (mustRender)
+                            {
+                                _htmlImage = HtmlRender.RenderToImage(str,
+                                    new Size(HtmlWindowUsableWidth, _currentLcdHeight + 20), Color.Black, App.CssData,
+                                    null, OnImageLoad);
+                            }
+
+                            if (_htmlImage != null)
+                            {
+                                graphics.DrawImage(_htmlImage, new Rectangle(new Point(HtmlWindowXOffset, 0),
+                                        new Size(HtmlWindowUsableWidth, _htmlWindowHeight + 20)),
+                                    new Rectangle(new Point(0, _currentLcdYOffset),
+                                        new Size(HtmlWindowUsableWidth, _htmlWindowHeight + 20)),
+                                    GraphicsUnit.Pixel);
+                            }
+                        }
+
+                        if (_currentLcdHeight > _htmlWindowHeight)
+                        {
+                            var scrollThumbHeight = _htmlWindowHeight / (double)_currentLcdHeight * ScrollBarHeight;
+                            var scrollThumbYOffset = _currentLcdYOffset / (double)_currentLcdHeight * ScrollBarHeight;
+
+                            graphics.DrawRectangle(_scrollPen, new Rectangle(new Point(_htmlWindowWidth - 9, 2),
+                                                               new Size(5, (int)ScrollBarHeight)));
+
+                            graphics.FillRectangle(_scrollBrush, new Rectangle(new Point(_htmlWindowWidth - 9, 2 + (int)scrollThumbYOffset),
+                                new Size(5, 1 + (int)scrollThumbHeight)));
+
+                        }
+                        
+
+
+                        if (mustRender)
+                        {
+                            var cardcaptionstr =
+                                Engine.Razor.Run("cardcaption.cshtml", null, new
+                                {
+                                    CurrentCard = CurrentCard
+                                });
+
+                            _cardcaptionHtmlImage = HtmlRender.RenderToImage(cardcaptionstr,
+                                new Size(HtmlWindowUsableWidth, 26), Color.Black, App.CssData, null,
+                                null);
+                        }
+
+                        if (_cardcaptionHtmlImage != null)
+                        {
+                            graphics.DrawImage(_cardcaptionHtmlImage, HtmlWindowXOffset, 0);
+                        }
+
+                        SendImageToFip(DEFAULT_PAGE, fipImage);
+
+                        if (_initOk)
+                        {
+                            for (uint i = 2; i <= 6; i++)
+                            {
+                                SetLed(i, false);
+                            }
+
+                            SetLed(5, true);
+                            SetLed(6, true);
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
